9eb98235d9c88504cadd5bb2c5b8e5f036c5ee20
ALTR <chuan.khuna@gmail.com>
2025-06-12 16:14:37 +0700

    Refactor: rename palettes.py to palette_manager.py and styles.py to style_manager.py; move config files to palettes_conf/ and styles_conf/; update imports for clarity and avoid naming conflicts.

--- src/altr/visualisation/__init__.py
+++ src/altr/visualisation/__init__.py
@@ -1,58 +1,9 @@
-import os
-import matplotlib.pyplot as plt
-import importlib
-import seaborn as sns
+from .style_manager import set_style, available_styles
+from .palette_manager import set_seaborn_palette, available_palettes
 
-STYLE_DIR = os.path.join(os.path.dirname(__file__), "styles")
-
-
-def _list_styles() -> list[str]:
-    """
-    List available styles
-    """
-
-    files = os.listdir(STYLE_DIR)
-    style_names = [filename.split(".mplstyle")[0] for filename in files]
-
-    return style_names
-
-
-def _list_palettes() -> list[str]:
-    """
-    List available palettes
-    """
-
-    files = os.listdir(os.path.join(os.path.dirname(__file__), "palettes"))
-    palette_names = [filename.split(".py")[0] for filename in files]
-    palette_names = [name for name in palette_names if not name.startswith("_")]
-
-    return palette_names
-
-
-available_styles = _list_styles()
-available_palettes = _list_palettes()
-
-
-def set_style(name: str) -> None:
-    """
-    Set matplotlib style
-
-
-    Args:
-        name (str): name of the style
-    """
-
-    if name not in available_styles:
-        # fallback to matplotlib style
-        plt.style.use(name)
-
-    file = f"{name}.mplstyle"
-    plt.style.use(os.path.join(STYLE_DIR, file))
-
-
-def set_seaborn_palette(name: str, scheme: str = "main_colors") -> None:
-    # dynamically import the style from palettes
-    module = importlib.import_module(f"altr.visualisation.palettes.{name}")
-    colors = getattr(module, scheme, None).values()
-
-    sns.set_palette(colors)
+__all__ = [
+    "set_style",
+    "available_styles",
+    "set_seaborn_palette",
+    "available_palettes",
+]
--- tests/test_altr.py
+++
@@ -1,4 +0,0 @@
-def test_greet_function():
-    from altr import greet
-
-    assert isinstance(greet.hello(), str)
